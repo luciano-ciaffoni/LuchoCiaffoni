9bfa3cac53e5d3674640f4fb07210befc9e104d0
alumno <alumno@Instituto.local>
2025-03-18 09:51:44 -0300

    if y else
    18/3/2025

--- java.js
+++ java.js
@@ -1,4 +1,13 @@
-let AñoNacimiento = 2008
-let AñoActual = 2025
-let Edad = AñoActual - AñoNacimiento
-alert ("mi edad es"+ Edad)
+//let AñoNacimiento = 2008
+//let AñoActual = 2025
+//let Edad = AñoActual - AñoNacimiento
+//alert ("mi edad es"+ Edad)
+
+let EdadIf = prompt ("edad?")
+
+if (EdadIf >= 18) {
+    alert ("pasa")
+} else {
+    alert ("no pasas")
+    
+}
