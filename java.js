cf1e8a735885acd3ab42b0a9d38c3cb42b8a94be
alumno <alumno@Instituto.local>
2025-03-19 08:22:50 -0300

    19/3/2025

--- java.js
+++ java.js
@@ -2,12 +2,27 @@
 //let AñoActual = 2025
 //let Edad = AñoActual - AñoNacimiento
 //alert ("mi edad es"+ Edad)
+let Pais = prompt ("sos de Argentina")
+let Provincia = prompt ("sos de La Pampa")
 
-let EdadIf = prompt ("edad?")
+if ((Pais =="si") && (Provincia =="si")) {
+    alert ("yo tambie ")
+} else {
+    alert ("yo soy de Argentina y vivo en La Pampa")
+}
+
+/*let EdadIf = prompt ("edad?")
 
 if (EdadIf >= 18) {
     alert ("pasa")
 } else {
     alert ("no pasas")
+    
+}*/
+
+
+if (condition) {
+    
+} else {
     
 }
